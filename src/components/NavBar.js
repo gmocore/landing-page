4640e15668a1d33075dd9b2a60ef2a11f84d44bd
Gerritt Black <you@example.comgmocore@gmail.com>
2021-01-30 12:58:34 -0700

    nav-hover animation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
         <Navbar bg="white" variant="white" className="container-fluid sticky-top" id="nav-container">
             <div className="nav-wrapper container">
 
-                <Navbar.Brand className="ml-4" width="100%">
+                <Navbar.Brand className="ml-4 nav-link" width="100%">
                     <Link to="/">
                         <img
                             src={logo}
@@ -17,14 +17,16 @@ const NavBar = () => {
                     </Link>
                 </Navbar.Brand>
                 <div className="link-container ml-auto mr-3 d-flex">
-                    <Nav.Link>
-                        <Link to="/" className="me-2 nav-link text-secondary letter-spacing">Home</Link>
+                    <Nav.Link to="/" className="link-outer me-2 nav-link letter-spacing ">
+                        Home
+                        {/* <Link to="/" className="me-2 nav-link text-secondary letter-spacing link-inner">Home</Link> */}
                     </Nav.Link>
                     {/* <Nav.Link>
                         <Link to="/about">About</Link>
                     </Nav.Link> */}
-                    <Nav.Link>
-                        <Link to="/contact" className="nav-link text-secondary letter-spacing">Contact</Link>
+                    <Nav.Link to="/contact" className="link-outer me-2 nav-link letter-spacing ">
+                        Contact
+                        {/* <Link to="/contact" className="nav-link text-secondary letter-spacing">Contact</Link> */}
                     </Nav.Link>
                 </div>
             </div>
